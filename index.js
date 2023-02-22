46d085cf57755e6d146f4423af77e98a463a9d38
Designer's Image <designersimage.io@gmail.com>
2023-02-22 12:07:13 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ |-----------------------------------------------------------------------
+ | EreborJS
+ |-----------------------------------------------------------------------
+ | 
+ | 
+ | @package ereborjs
+ | @author Jonathan Wheeler <jonathan@desginersimage.io>
+ */
+
+function helloNpm() {
+  return "hello NPM"
+}
+
+module.exports = helloNpm
